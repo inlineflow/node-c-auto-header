879ae04ea20f33312c9bd79b2758e6cf23968e8d
Danil Samsonov <thrashdev@protonmail.com>
2025-12-28 12:38:56 +0600

    WIP; working on CLI

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const Parser = require("tree-sitter")
 const C_Lang = require("tree-sitter-c")
-const crypto = require("node:crypto")
 const path = require("node:path")
 const fs = require("fs")
-const { writeFileSync } = require("node:fs")
 const parser = new Parser()
 parser.setLanguage(C_Lang)
 
-const hashString = (s) => {
-  return crypto.createHash("sha1").update(s).digest("hex");
+const is_dir = (path) => {
+  try {
+    const stats = fs.statSync(path)
+    return stats.isDirectory();
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      return false;
+    }
+
+    throw error;
+  }
 }
+
+const args = process.argv.slice(2)
+if (args.length === 0) {
+  console.log("Provide a path to the root directory of your C source files.")
+  process.exit(1)
+}
+
+const dir = args[0]
+if (!is_dir(dir)) {
+  console.log("Provided path is not a directory.")
+  console.log("Provide a path to the root directory of your C source files.")
+  process.exit(1)
+}
+
+try {
+  fs.accessSync(dir)
+} catch (error) {
+  console.log("Couldn't access path")
+  process.exit(1)
+}
+
+const c_files = fs.readdirSync(dir, { recursive: true })
+  .map(fname => path.join(dir, fname))
+  .filter(i => fs.lstatSync(i).isFile())
+  .filter(i => i.endsWith(".c"))
+  .map(i => path.resolve(i))
+console.log(c_files)
 
 const processFile = (filepath, query) => {
   const fileContents = fs.readFileSync(filepath).toString()
-  // console.log(fileContents)
   const tree = parser.parse(fileContents)
 
   const matches = query.matches(tree.rootNode)
@@ -21,17 +54,12 @@ const processFile = (filepath, query) => {
   for (const match of matches) {
     const t1 = match.captures.map(c => tree.getText(c.node))
     signature_text.push(t1.join(" "))
-    // console.log(t1)
   }
 
   const signatures = signature_text.map(st => st + ";")
   console.log(signatures)
-  const signature_hashes = signatures.map(sig => ({
-    signature: sig,
-    hash: hashString(sig),
-  }))
 
-  return signature_hashes
+  return signatures
 }
 
 const c_query = new Parser.Query(C_Lang,
@@ -46,78 +74,48 @@ const h_query = new Parser.Query(C_Lang,
   declarator: (function_declarator ) @decl 
   )`);
 
-const filepath = "../DSA_C/base.c"
-const dir_name = path.dirname(filepath)
-
-// console.log(dir_name)
-const base_name = path.basename(filepath)
-const extensionless_name = base_name.slice(0, base_name.lastIndexOf("."))
-const header_filename = path.join(dir_name, extensionless_name + ".h")
-
-const c_file_hashes = processFile(filepath, c_query)
-console.log(header_filename)
-const h_file_hashes = processFile(header_filename, h_query)
-
-console.log(c_file_hashes);
-console.log(h_file_hashes);
-
-// const data = []
-// for (const c_hash of c_file_hashes) {
-//   for (const h_hash of h_file_hashes) {
-//     if (c_hash.hash === h_hash.hash) {
-//       data.push(c_hash)
-//     }
-//   }
-// }
-
-const writeHeader = (filepath, hashes) => {
+const writeHeader = (filepath, signatures) => {
   const fileContents = fs.readFileSync(filepath).toString()
   const lines = fileContents.split("\n")
-  // const result = ""
 
   const tree = parser.parse(fileContents)
 
   const matches = h_query.matches(tree.rootNode)
   const sets = []
   for (const m of matches) {
-    // console.log(matches.map(m => m.captures))
     sets.push(new Set(m.captures.map(c => c.node.startPosition.row)))
   }
 
   for (const s of sets) {
     for (const e of s) {
       lines.splice(e, 1)
-      // console.log(lines[e])
-      // if (hashes.map(h => h.signature).includes(lines[e])) {
-      //   console.log("Match: ", lines[e])
-      // } else {
-      //   // lines[e] = ""
-      //   lines.splice(e, 1)
-      // }
     }
   }
 
-  const result = [...lines, ...hashes.map(h => h.signature)]
+  const result = [...lines, ...signatures]
+  fs.writeFileSync(header_filename, result.join("\n"))
 
   return result
 }
 
-const data = c_file_hashes
-console.log("hashes: ")
-console.log(data)
-const lines = writeHeader(header_filename, data)
-console.log(lines)
+const generateHeader = (filepath) => {
+  const dir_name = path.dirname(filepath)
+  const base_name = path.basename(filepath)
+  const extensionless_name = base_name.slice(0, base_name.lastIndexOf("."))
+  const header_filename = path.join(dir_name, extensionless_name + ".h")
+  const c_file_sigs = processFile(filepath, c_query)
+  const lines = writeHeader(header_filename, c_file_sigs)
+}
 
-// console.log(signatures)
-// console.log(signature_hashes)
-
-// const result = `/* this header has been generated automatically
-// do not edit
-// generated at: ${new Date().toISOString()}
-// corresponding C file: ${base_name} */
-//
-// ${signature_text.map(st => st + ";").join("\n")}`
+for (const c_file of c_files) {
+  generateHeader(c_file)
+}
 
 
 
-// fs.writeFileSync(new_filename, result)
+
+
+
+
+
+console.log(process.argv.slice(2))
